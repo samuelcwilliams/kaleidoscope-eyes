9c279db6aa341ff5358b490fc834b4749ee9cc00
Sam Williams <samcwilliams67@gmail.com>
2022-06-13 22:16:15 -0500

    adding a new demo file

---
+++ file.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import cv2
+
+# defining the image
+image = cv2.imread("image.jpg")
+
+# defining the index of the .csv
+index=["color", "color_name", "hex", "R", "G", "B"]
+
+# defining the colors
+colors = pd.read_csv('colors.csv', names=index, header=None)
+
+# defining global variables
+clicked = False
+r = g =b = xpos = ypos = 0
+
+# this function uses the colors.csv file that was loaded to return the color name and RGB value
+def recognize_color(R, G, B):
+    minimum = 10000
+    for i in range(len(colors)):
+        d = abs(R- int(colors.loc[i, "R"])) + abs(G- int(colors.loc[i, "G"])) + abs(B- int(colors.loc[i, "B"]))
+        if(d<=minimum):
+            minimum = d
+            colorname = colors.loc[i, "color_name"]
+    return colorname
+
+# this function triggers the mouse_click event 
+def mouse_click(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b, g, r, xpos, ypos, clicked
+        clicked = True
+        xpos = x
+        ypos = y
+        b, g, r = image[y, x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
+# this section creates the window with the image inside
+cv2.namedWindow('Kaleidoscope Eyes')
+cv2.setMouseCallback('Kaleidoscope Eyes', mouse_click)
+
+# while loop that starts the application and keeps it running
+while(1):
+    cv2.rectangle(image,(20,20), (750,60), (b,g,r), -1)
+    text = recognize_color(r,g,b) + ' R='+ str(r) + ' G='+str(g) + ' B='+ str(b)
+    cv2.putText(image, text, (50,50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+
+    if (r+g+b>=600):
+        cv2.putText(image, text, (50, 50), 2, 0.8, (0,0,0), 2, cv2.LINE_AA)
+        clicked=False
+
+    if cv2.waitKey(20) & 0xFF ==27:
+        break
+
+# closes window
+cv2.destroyAllWindows()
