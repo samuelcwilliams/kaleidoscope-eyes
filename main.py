23c44367ce2d4422ac6aa568798fa01bf9474693
Sam Williams <samcwilliams67@gmail.com>
2022-06-14 14:44:24 -0500

    added automatic copying feature

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
 import cv2
+import pyperclip
 
 # defining the image
-your_image = input("Please enter your image name: ")
-image = cv2.imread(your_image)
+# your_image = input("Please enter your image name: ")
+image = cv2.imread(input("Please enter your image: "))
 
 # defining the index of the .csv
 index=["color", "color_name", "hex", "R", "G", "B"]
@@ -38,6 +39,10 @@ def mouse_click(event, x, y, flags, param):
         g = int(g)
         r = int(r)
 
+# this section copies the rgb(r, g, b) value to the clipboard automatically
+def copy_rgb(r, g, b):
+    pyperclip.copy('rgb(' + str(r) + ',' + str(g) + ',' + str(b) + ')')
+
 # this section creates the window with the image inside
 cv2.namedWindow('Kaleidoscope Eyes')
 cv2.setMouseCallback('Kaleidoscope Eyes', mouse_click)
@@ -49,6 +54,7 @@ while(1):
         cv2.rectangle(image,(20,20), (750,60), (b,g,r), -1)
         text = recognize_color(r,g,b) + ' R='+ str(r) + ' G='+str(g) + ' B='+ str(b)
         cv2.putText(image, text, (50,50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+        copy_rgb(r, g, b)
 
     if (r+g+b>=600):
         cv2.putText(image, text, (50, 50), 2, 0.8, (0,0,0), 2, cv2.LINE_AA)
